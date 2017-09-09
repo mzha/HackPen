a45933d7853592a29edc1c6ba8eb61b74c177ec1
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-09-09 01:51:18 -0700

    progress

---
+++ app/src/main/java/com/example/androidthings/myproject/Logging/ChunkAllocator.java
@@ -0,0 +1,15 @@
+package com.example.androidthings.myproject.Logging;
+
+/**
+ * Created by Arthur on 9/9/17.
+ * Allocates new chunks as quickly as possible while reasonable
+ */
+
+public class ChunkAllocator implements Runnable {
+
+    private Queue<>
+    @Override
+    public void run() {
+
+    }
+}
